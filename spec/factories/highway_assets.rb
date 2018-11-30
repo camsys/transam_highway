62536bab47e187105f537252751baf6f4417a657
mathmerized <mathmerized@gmail.com>
2018-11-30 14:06:42 -0500

    add highway asset and component

---
+++ spec/factories/highway_assets.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :highway_asset do
+    
+  end
+end
---
+++ spec/models/highway_asset_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe HighwayAsset, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
