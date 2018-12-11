5798610bd7ffb6e5d8031ae02e6c0f6ffff3c4c5
Xudong Liu <xudong.camsys@gmail.com>
2018-12-11 09:49:57 -0500

    BMCDOT-62 add config to whitelist allowable lookup tables for associations API

---
+++ config/initializers/transam.rb
@@ -0,0 +1,15 @@
+Rails.application.config.rails_admin_highway_lookup_tables = [
+  'BridgeAppraisalRatingType',
+  'BridgeConditionRatingType',
+  'ChannelConditionType',
+  'DeckProtectionType',
+  'DeckStructureType',
+  'DesignConstructionType',
+  'MembraneType',
+  'OperationalStatusType',
+  'ScourCriticalBridgeType',
+  'StrahnetDesignationType',
+  'StructureMaterialType',
+  'WearingSurfaceType',
+  'route_signing_prefix'
+]
