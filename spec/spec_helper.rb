52d92c2ba9c5ccef6d6b69dddaa08bbbf90aaf2e
mathmerized <mathmerized@gmail.com>
2018-12-11 10:57:26 -0500

    more test config cleanup, also add Rails.config so belongs_to not required by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     #expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+  #end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
@@ -35,7 +35,7 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     #mocks.verify_partial_doubles = true
-  end
+  #end
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
