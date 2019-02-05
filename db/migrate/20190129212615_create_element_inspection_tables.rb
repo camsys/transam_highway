d8d3bbc9145cc3e0be3a3000bd273bfa286ba2f9
mathmerized <mathmerized@gmail.com>
2019-02-05 09:30:35 -0500

    seed data for element definition

--- db/migrate/20190129212615_create_element_inspection_tables.rb
+++ db/migrate/20190129212615_create_element_inspection_tables.rb
@@ -28,7 +28,6 @@ class CreateElementInspectionTables < ActiveRecord::Migration[5.2]
 
     create_table :element_classifications do |t|
       t.string :name
-      t.string :code
       t.boolean :active
     end
 
