a02ee5799da1ce9755f673479c3e3f5a53166413
mathmerized <mathmerized@gmail.com>
2019-02-07 11:48:10 -0500

    BMCDOT-189 cleanup migration, no indices

--- db/migrate/20190207153152_create_bridge_conditions.rb
+++ db/migrate/20190207153152_create_bridge_conditions.rb
@@ -2,24 +2,24 @@ class CreateBridgeConditions < ActiveRecord::Migration[5.2]
   def change
     create_table :bridge_conditions do |t|
 
-      t.references :deck_condition_rating_type
-      t.references :superstructure_condition_rating_type
-      t.references :substructure_condition_rating_type
+      t.references :deck_condition_rating_type, index: false
+      t.references :superstructure_condition_rating_type, index: false
+      t.references :substructure_condition_rating_type, index: false
 
-      t.references :structural_appraisal_rating_type
-      t.references :deck_geometry_appraisal_rating_type
-      t.references :underclearance_appraisal_rating_type
-      t.references :waterway_appraisal_rating_type
-      t.references :approach_alignment_appraisal_rating_type
+      t.references :structural_appraisal_rating_type, index: false
+      t.references :deck_geometry_appraisal_rating_type, index: false
+      t.references :underclearance_appraisal_rating_type, index: false
+      t.references :waterway_appraisal_rating_type, index: false
+      t.references :approach_alignment_appraisal_rating_type, index: false
 
-      t.references :operational_status_type
-      t.references :channel_condition_type
-      t.references :scour_critical_bridge_type
+      t.references :operational_status_type, index: false
+      t.references :channel_condition_type, index: false
+      t.references :scour_critical_bridge_type, index: false
 
-      t.references :railings_safety_type
-      t.references :transitions_safety_type
-      t.references :approach_rail_safety_type
-      t.references :approach_rail_end_safety_type
+      t.references :railings_safety_type, index: false
+      t.references :transitions_safety_type, index: false
+      t.references :approach_rail_safety_type, index: false
+      t.references :approach_rail_end_safety_type, index: false
     end
 
     remove_column :bridges, :deck_condition_rating_type_id
@@ -36,7 +36,7 @@ class CreateBridgeConditions < ActiveRecord::Migration[5.2]
     remove_column :bridges, :channel_condition_type_id
     remove_column :bridges, :scour_critical_bridge_type_id
 
-    add_column :inspections, :inspectionible, polymorphic: true, index: {name: :inspectionible_idx}, after: :object_key
+    add_reference :inspections, :inspectionible, polymorphic: true, index: {name: :inspectionible_idx}, after: :object_key
 
 
     create_table :feature_safety_types do |t|
