1d4555f97ff8df6d7c696fc897df1e3e849dd57a
mathmerized <mathmerized@gmail.com>
2019-02-11 13:04:22 -0500

    validate uniqueness of element and defect definition numbers

--- app/models/defect_definition.rb
+++ app/models/defect_definition.rb
@@ -1,6 +1,8 @@
 class DefectDefinition < ApplicationRecord
   include TransamObjectKey
 
+  validates :number, :uniqueness => true
+
   def to_s
     number.to_s
   end
--- app/models/element_definition.rb
+++ app/models/element_definition.rb
@@ -6,6 +6,8 @@ class ElementDefinition < ApplicationRecord
   belongs_to :assembly_type
   belongs_to :element_classification
 
+  validates :number, :uniqueness => true
+
   def to_s
     number.to_s
   end
