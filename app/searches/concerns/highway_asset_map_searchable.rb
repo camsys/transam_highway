c5dc3caff8420cb27e4cb726365278963053ed2b
Xudong Liu <xudong.camsys@gmail.com>
2019-03-01 15:30:46 -0500

    BMCDOT-240 add owner filter

--- app/searches/concerns/highway_asset_map_searchable.rb
+++ app/searches/concerns/highway_asset_map_searchable.rb
@@ -6,7 +6,7 @@ module HighwayAssetMapSearchable
 
   included do
 
-    attr_accessor :region_code, :structure_status_type_code
+    attr_accessor :region_code, :structure_status_type_code, :owner_id
 
   end
 
@@ -30,12 +30,11 @@ module HighwayAssetMapSearchable
       # table alias being used to avoid the case when this table is being joined in another place
       join_sql = <<-SQL 
         LEFT JOIN highway_structures 
-          AS base_highway_structures
-          ON base_highway_structures.id = transam_assets.transam_assetible_id AND transam_assets.transam_assetible_type = 'HighwayStructure'
+          ON highway_structures.id = transam_assets.transam_assetible_id AND transam_assets.transam_assetible_type = 'HighwayStructure'
         LEFT JOIN regions 
-          ON base_highway_structures.region_id = regions.id
+          ON highway_structures.region_id = regions.id
         LEFT JOIN structure_status_types 
-          ON base_highway_structures.structure_status_type_id = structure_status_types.id
+          ON highway_structures.structure_status_type_id = structure_status_types.id
       SQL
 
       @highway_klass ||= @klass.joins(join_sql)
@@ -54,6 +53,10 @@ module HighwayAssetMapSearchable
   def structure_status_type_code_conditions
     clean_structure_status_type_codes = remove_blanks(structure_status_type_code)
     highway_klass.where("structure_status_types.code": clean_structure_status_type_codes) unless clean_structure_status_type_codes.empty?
+  end
+
+  def owner_id_conditions
+    highway_klass.where("highway_structures.owner_id": owner_id) unless owner_id.blank?
   end
 
 end
