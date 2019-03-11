19c3417582bc55c47557713d5b2dad5c18c86e49
mathmerized <mathmerized@gmail.com>
2019-03-11 09:54:16 -0400

    fix migration after clause

--- db/migrate/20190308151238_add_guid_transam_assets.rb
+++ db/migrate/20190308151238_add_guid_transam_assets.rb
@@ -2,7 +2,7 @@ class AddGuidTransamAssets < ActiveRecord::Migration[5.2]
   def change
     add_column :transam_assets, :guid, :string, after: :object_key
     add_column :inspections, :guid, :string, after: :object_key
-    add_column :bridge_conditions, :guid, :string, after: :object_key
+    add_column :bridge_conditions, :guid, :string, after: :id
     add_column :elements, :guid, :string, after: :object_key
     add_column :defects, :guid, :string, after: :object_key
     add_column :images, :guid, :string, after: :object_key
