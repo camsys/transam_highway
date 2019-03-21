e4c52ab6c994bd5ef997af0d0fb0b947941eb0f0
mathmerized <mathmerized@gmail.com>
2019-03-21 11:08:59 -0400

    typo

--- db/migrate/20190321132910_rename_bridge_to_bridge_like.rb
+++ db/migrate/20190321132910_rename_bridge_to_bridge_like.rb
@@ -1,4 +1,4 @@
-class RenameBridgeConditionToBridgeLikeCondition < ActiveRecord::Migration[5.2]
+class RenameBridgeToBridgeLike < ActiveRecord::Migration[5.2]
   def change
     rename_table :bridges, :bridge_likes
     rename_table :bridge_conditions, :bridge_like_conditions
