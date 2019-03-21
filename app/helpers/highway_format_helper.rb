573ae01ef12fde833b48be2c189e5c035372068d
W. Scott Meeks <smeeks@camsys.com>
2019-03-21 17:44:48 -0400

    BMCDOT-242 create roadways tables

--- app/helpers/highway_format_helper.rb
+++ app/helpers/highway_format_helper.rb
@@ -6,4 +6,12 @@ module HighwayFormatHelper
   def reference_label(table_name, field_name, default_label)
     FieldReference.get_label(table_name, field_name) || default_label 
   end
+
+  def suppress_values(value, suppress_all_zeroes: true, suppress_list: [-1, '-1', '_'], &block)
+    if (suppress_all_zeroes && value.to_i == 0) || suppress_list.include?(value)
+      '-'
+    else
+      block_given? ? yield(value) : value
+    end
+  end
 end
