d6bcb53736e618661ca285992096126f7aa47d3e
mathmerized <mathmerized@gmail.com>
2019-03-22 10:17:47 -0400

    add and use get_typed_inspection and get_typed_asset instead of specific methods

--- app/models/bridge_condition.rb
+++ app/models/bridge_condition.rb
@@ -6,4 +6,30 @@ class BridgeCondition < BridgeLikeCondition
 
   belongs_to :underclearance_appraisal_rating_type, class_name: 'BridgeAppraisalRatingType'
 
+  after_save :update_calculated_condition
+
+  def calculated_condition
+    case [deck_condition_rating_type&.value, superstructure_condition_rating_type&.value,
+          substructure_condition_rating_type&.value].compact.min
+      when 0..4
+        'poor'
+      when 5..6
+        'fair'
+      when 7..9
+        'good'
+      else
+        'unknown'
+    end
+  end
+
+  def update_calculated_condition
+    if self.deck_condition_rating_type_id_changed? ||
+        self.superstructure_condition_rating_type_id_changed? ||
+        self.substructure_condition_rating_type_id_changed?
+      # get bridge
+      br = TransamAsset.get_typed_asset(self.highway_structure)
+      br.try(:set_calculated_condition!) if br
+    end
+  end
+
 end
--- app/models/bridge_like_condition.rb
+++ app/models/bridge_like_condition.rb
@@ -17,36 +17,12 @@ class BridgeLikeCondition < ApplicationRecord
   belongs_to :approach_rail_safety_type, class_name: 'FeatureSafetyType'
   belongs_to :approach_rail_end_safety_type, class_name: 'FeatureSafetyType'
 
-  after_save :update_calculated_condition
-
-  def calculated_condition
-    case [deck_condition_rating_type&.value, superstructure_condition_rating_type&.value,
-          substructure_condition_rating_type&.value].compact.min
-    when 0..4
-      'poor'
-    when 5..6
-      'fair'
-    when 7..9
-      'good'
-    else
-      'unknown'
-    end
-  end
-
   def bridge
-    highway_structure.very_specific
+    TransamAsset.get_typed_asset(highway_structure)
   end
 
   private
 
-  def update_calculated_condition
-    if self.deck_condition_rating_type_id_changed? || 
-      self.superstructure_condition_rating_type_id_changed? ||
-      self.substructure_condition_rating_type_id_changed?
-      # get bridge
-      br = self.highway_structure.try(:very_specific)
-      br.try(:set_calculated_condition!) if br
-    end
-  end
+
 
 end
