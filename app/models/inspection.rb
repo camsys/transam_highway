f727a1486ce993769b19bf5d85599753f52767c7
mathmerized <mathmerized@gmail.com>
2019-03-29 14:03:23 -0400

    pending workflow transitions BMCDOT-115

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -26,4 +26,70 @@ class Inspection < ApplicationRecord
       inspection
     end
   end
+
+  def self.transam_workflow_transitions
+    [
+        {event_name: 'make_ready', from_state: 'open', to_state: 'ready', guard: :allowed_to_make_ready, can: :can_make_ready},
+
+        {event_name: 'assign', from_state: 'ready', to_state: 'assigned', guard: :allowed_to_assign, can: :can_assign},
+
+        {event_name: 'send_to_field', from_state: 'assigned', to_state: 'in_field'},
+
+        {event_name: 'start', from_state: 'in_field', to_state: 'in_progress', can: :can_start},
+        {event_name: 'save', from_state: 'in_progress', to_state: 'in_field', can: :can_start},
+
+        {event_name: 'send', from_state: ['in_field', 'in_progress'], to_state: 'draft', can: :can_start},
+
+        {event_name: 'qc_review', from_state: 'draft', to_state: 'qc_reviewed', can: :can_qc},
+
+        {event_name: 'qa_review', from_state: 'qc_reviewed', to_state: 'qa_reviewed', can: :can_qa},
+        {event_name: 'qc_submit', from_state: 'qc_reviewed', to_state: 'submitted', guard: :allowed_to_qc_submit, can: :can_submit},
+        {event_name: 'qc_revert', from_state: 'qc_reviewed', to_state: 'draft', can: :can_qc},
+
+        {event_name: 'qa_submit', from_state: 'qa_reviewed', to_state: 'submitted', can: :can_submit},
+        {event_name: 'revert', from_state: 'qa_reviewed', to_state: 'qc_review', can: :can_qa},
+        {event_name: 'qa_revert', from_state: 'qa_reviewed', to_state: 'draft', can: :can_qa},
+
+        {event_name: 'finalize', from_state: 'submitted', to_state: 'final', can: :can_finalize}
+
+    ]
+  end
+
+  def allowed_to_make_ready
+    #inspection_group.present? && inspection_team.present?
+    true
+  end
+  def can_make_ready(user)
+    user.try(:is_inspection_admin)
+  end
+
+  def allowed_to_assign
+    inspectors.count > 0
+  end
+  def can_assign(user)
+    user.try(:is_editor)
+  end
+
+  def can_start(user)
+    inspectors.include? user
+  end
+
+  def can_qc(user)
+    user.try(:is_qc_reviewer)
+  end
+  def can_qa(user)
+    user.try(:is_qa_reviewer)
+  end
+
+  def allowed_to_qc_submit
+    # can skip QA
+    true
+  end
+  def can_submit(user)
+    user.try(:is_submitter)
+  end
+  def can_finalize(user)
+    user.try(:is_finalizer)
+  end
+
 end
