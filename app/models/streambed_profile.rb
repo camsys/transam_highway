3bf0c1e54f7a03534edb849becc2a57ef4b845ed
mathmerized <mathmerized@gmail.com>
2019-07-01 12:36:00 -0400

    BMCDOT-332 typo

--- app/models/streambed_profile.rb
+++ app/models/streambed_profile.rb
@@ -27,7 +27,7 @@ class StreambedProfile < ApplicationRecord
   # this profile instance might not have all those columns but these are all the columns it could have
   # on create, it pulls these columns. it doesn't update later if new columns are added later or changed
   def all_possible_distances
-    bridge_like.streambed_profiles.map{|x| x.streambed_profiles.pluck(:distance)}.flatten.uniq.sort
+    bridge_like.streambed_profiles.map{|x| x.streambed_profile_points.pluck(:distance)}.flatten.uniq.sort
   end
 
   def create_streambed_profile_points
