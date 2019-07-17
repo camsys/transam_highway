5611579806c32d042deb297c2083dc6154ceaa41
mathmerized <mathmerized@gmail.com>
2019-07-17 15:37:17 -0400

    fix org role mappings BMCDOT-369

--- db/data_migrations/20190716170016_add_highway_team_organization_type.rb
+++ db/data_migrations/20190716170016_add_highway_team_organization_type.rb
@@ -12,5 +12,8 @@ class AddHighwayTeamOrganizationType < ActiveRecord::DataMigration
 
     OrganizationType.find_by(class_name: 'HighwayAuthority').update!(roles: 'manager,inspector')
     OrganizationType.where.not(class_name: 'HighwayAuthority').update_all(roles: 'inspector')
+
+    Organization.includes(:organization_type).where(organization_types: {class_name: 'HighwayAuthority'}).each{|x| OrganizationRoleMapping.create!(organization_id: x.id, role_id: Role.find_by(name: 'manager').id)}
+    Organization.all.each{|x| OrganizationRoleMapping.create!(organization_id: x.id, role_id: Role.find_by(name: 'inspector').id)}
   end
 end
