69c100b7b6c2a57f0c3b1187b2ede70a74f0ab21
Xudong Liu <xudong.camsys@gmail.com>
2019-07-22 09:44:06 -0400

    Make weather and temp optional

--- app/models/highway_structure.rb
+++ app/models/highway_structure.rb
@@ -163,7 +163,7 @@ class HighwayStructure < TransamAssetRecord
       Inspection.get_typed_inspection(inspections.where.not(state: 'final').first)
     elsif inspections.count > 0
       old_insp = Inspection.get_typed_inspection(last_closed_inspection)
-      new_insp = old_insp.deep_clone include: {elements: :defects}, except: [:object_key, :guid, :state, :event_datetime, :calculated_inspection_due_date, :qc_inspector_id, :qa_inspector_id, :routine_report_submitted_at, {elements: [:guid, {defects: [:object_key, :guid]}]}]
+      new_insp = old_insp.deep_clone include: {elements: :defects}, except: [:object_key, :guid, :state, :event_datetime, :weather, :temperature, :calculated_inspection_due_date, :qc_inspector_id, :qa_inspector_id, :routine_report_submitted_at, {elements: [:guid, {defects: [:object_key, :guid]}]}]
 
       old_insp.elements.where(id: old_insp.elements.distinct.pluck(:parent_element_id)).each do |old_parent_elem|
         new_parent_elem = new_insp.elements.select{|e| e.object_key == old_parent_elem.object_key}.first
