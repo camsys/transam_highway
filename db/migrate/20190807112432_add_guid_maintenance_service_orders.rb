96004c615b059384a1ae69bf662595f01abf0961
mathmerized <mathmerized@gmail.com>
2019-08-07 07:32:07 -0400

    guid column maintenance service orders

---
+++ db/migrate/20190807112432_add_guid_maintenance_service_orders.rb
@@ -0,0 +1,9 @@
+class AddGuidMaintenanceServiceOrders < ActiveRecord::Migration[5.2]
+  def change
+    if ActiveRecord::Base.configurations[Rails.env]['adapter'].include?('mysql2')
+      add_column :maintenance_service_orders, :guid, :string, limit: 36, index: true, after: :object_key
+    else
+      add_column :maintenance_service_orders, :guid, :uuid, index: true, after: :object_key
+    end
+  end
+end
