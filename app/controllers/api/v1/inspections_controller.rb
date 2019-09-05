eb60acb368d1a5e1a6ec58fffe8f5bdc97da8ea6
W. Scott Meeks <smeeks@camsys.com>
2019-09-04 21:06:44 -0400

    BMCDOT-423 fix typo

--- app/controllers/api/v1/inspections_controller.rb
+++ app/controllers/api/v1/inspections_controller.rb
@@ -235,7 +235,7 @@ class Api::V1::InspectionsController < Api::ApiController
               clean_params[:roadbed] = rbl_roadbed if rbl_roadbed
               clean_params[:number] = rbl_number if rbl_number
               if rbl_params[:no_restriction]
-                clean_params[:entry] = clean_params[:exit] = null
+                clean_params[:entry] = clean_params[:exit] = nil
               elsif rbl_params[:does_not_exist]
                 clean_params[:entry] = clean_params[:exit] = 0.0
               end
