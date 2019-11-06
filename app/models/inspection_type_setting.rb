75e82089296a8f6abd5ba14dabc0549d2f0b3510
mathmerized <mathmerized@gmail.com>
2019-11-06 10:23:18 -0500

    add due date for managing schedules and generating thru inspection generator

--- app/models/inspection_type_setting.rb
+++ app/models/inspection_type_setting.rb
@@ -8,12 +8,15 @@ class InspectionTypeSetting < ApplicationRecord
   belongs_to :inspection_type
   belongs_to :highway_structure, foreign_key: :transam_asset_id
 
+  attr_accessor :calculated_inspection_due_date
+
   def self.allowable_params
     [
         :inspection_type_id,
         :frequency_months,
         :description,
         :is_required,
+        :calculated_inspection_due_date,
         :_destroy
     ]
   end
