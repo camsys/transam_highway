a0988f0b6441d391b87a0d647f5717df822dfe36
W. Scott Meeks <smeeks@camsys.com>
2019-11-27 09:42:02 -0500

    BMCDOT-501 Shift MiscStructure from ancillary to bridge

--- app/models/misc_structure.rb
+++ app/models/misc_structure.rb
@@ -1,3 +1,6 @@
-class MiscStructure < AncillaryStructure
+class MiscStructure < Bridge
   default_scope { where(asset_subtype: AssetSubtype.joins(:asset_type).where(asset_types: {class_name: 'MiscStructure'})) }
+  def inspection_class
+    BridgeCondition
+  end
 end
