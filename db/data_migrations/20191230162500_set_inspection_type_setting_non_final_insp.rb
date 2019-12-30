325b1431e89be20ba6004179d4eee5b5ee7da7b8
mathmerized <mathmerized@gmail.com>
2019-12-30 12:39:53 -0500

    BMCDOT-580 make sure non final inspections have a insp type setting

---
+++ db/data_migrations/20191230162500_set_inspection_type_setting_non_final_insp.rb
@@ -0,0 +1,9 @@
+class SetInspectionTypeSettingNonFinalInsp < ActiveRecord::DataMigration
+  def up
+    Inspection.where.not(state: 'final').each do |insp|
+      if insp.inspection_type.can_be_recurring && insp.description.blank? # try to handle special (wont catch all)
+        insp.update!(inspection_type_setting: insp.highway_structure.inspection_type_settings.find_by(inspection_type: insp.inspection_type))
+      end
+    end
+  end
+end
