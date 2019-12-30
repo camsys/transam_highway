b879145b41dfcd19b5dbb887a2eccd9845df75d8
mathmerized <mathmerized@gmail.com>
2019-12-30 15:44:38 -0500

    defensive code

--- db/data_migrations/20191230162500_set_inspection_type_setting_non_final_insp.rb
+++ db/data_migrations/20191230162500_set_inspection_type_setting_non_final_insp.rb
@@ -1,7 +1,7 @@
 class SetInspectionTypeSettingNonFinalInsp < ActiveRecord::DataMigration
   def up
     Inspection.where.not(state: 'final').each do |insp|
-      if insp.inspection_type.can_be_recurring && insp.description.blank? # try to handle special (wont catch all)
+      if insp.inspection_type&.can_be_recurring && insp.description.blank? # try to handle special (wont catch all)
         insp.update!(inspection_type_setting: insp.highway_structure.inspection_type_settings.find_by(inspection_type: insp.inspection_type))
       end
     end
