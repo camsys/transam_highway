823d4cf854143d302437a6bd314c2f05d470881d
W. Scott Meeks <smeeks@camsys.com>
2020-01-21 15:20:09 -0500

    BMCDOT-586: Add asset_types

--- app/models/assembly_type.rb
+++ app/models/assembly_type.rb
@@ -1,5 +1,7 @@
 class AssemblyType < ApplicationRecord
 
+  has_and_belongs_to_many :asset_types
+  
   # All types that are available
   scope :active, -> { where(:active => true) }
 
@@ -7,4 +9,7 @@ class AssemblyType < ApplicationRecord
     name
   end
 
+  def as_json(options={})
+    super.merge!({"asset_types" => asset_types.pluck(:name)})
+  end
 end
