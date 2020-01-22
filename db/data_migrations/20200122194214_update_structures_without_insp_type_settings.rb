9076c7972a3fd44d4f33bebe271086c1ef7c6fa7
mathmerized <mathmerized@gmail.com>
2020-01-22 17:28:13 -0500

    data migration to fix structures without routine insp type setting

---
+++ db/data_migrations/20200122194214_update_structures_without_insp_type_settings.rb
@@ -0,0 +1,7 @@
+class UpdateStructuresWithoutInspTypeSettings < ActiveRecord::DataMigration
+  def up
+    HighwayStructure.joins('LEFT JOIN inspection_type_settings ON inspection_type_settings.transam_asset_id = highway_structures.id').where(inspection_type_settings: {transam_asset_id: nil}).each do |asset|
+      asset.inspection_type_settings.create!(is_required: true, inspection_type: InspectionType.find_by(name: 'Routine'), frequency_months: (['HighwaySign', 'HighwaySignal', 'HighMastLight'].include?(asset.asset_type.class_name) ? 48 : 24))
+    end
+  end
+end
