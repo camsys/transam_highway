07b3ed601989762077ab90aa1bdb183bd3e0505d
mathmerized <mathmerized@gmail.com>
2020-01-23 10:38:10 -0500

    BMCDOT-550 defensive check if no assigned version yet

--- app/models/highway_structure.rb
+++ app/models/highway_structure.rb
@@ -164,7 +164,7 @@ class HighwayStructure < TransamAssetRecord
   end
 
   def assigned_version
-    return assigned_inspection_version.reify(belongs_to: true)&.highway_structure&.version || assigned_inspection_version.reify(belongs_to: true)&.highway_structure
+    return assigned_inspection_version&.reify(belongs_to: true)&.highway_structure&.version || assigned_inspection_version&.reify(belongs_to: true)&.highway_structure
   end
 
   def assigned_inspection_version
