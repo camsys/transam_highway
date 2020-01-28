05fa1c68892a7372b91e536ae49e386de4700bac
mathmerized <mathmerized@gmail.com>
2020-01-28 15:57:23 -0500

    add config necessary for paper trail auditing to dummy test app

---
+++ spec/dummy/config/initializers/transam_highway_extensions.rb
@@ -0,0 +1,9 @@
+Rails.application.config.inspection_audit_changes = ['bridge_like_conditions.scour_critical_bridge_type_id', 'highway_structures.structure_status_type_id',
+                                                     'highway_structures.inspection_program_id',
+                                                     'highway_structures.owner_id', 'bridge_likes.calculated_condition','bridge_like_conditions.deck_condition_rating_type_id',
+                                                     'bridge_like_conditions.superstructure_condition_rating_type_id',
+                                                     'bridge_like_conditions.substructure_condition_rating_type_id',
+                                                     'bridge_like_conditions.culvert_condition_type_id',
+                                                     'bridge_like_conditions.ancillary_condition_type_id',
+                                                     'bridge_like_conditions.operational_status_type_id', 'bridge_likes.operating_rating_method_type_id',
+                                                     'bridge_likes.inventory_rating_method_type_id', 'inspection_type_settings.inspection_frequency']
