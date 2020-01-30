60410eb8a2fbb553b1d5607d8bef6ef4e9990d8a
W. Scott Meeks <smeeks@camsys.com>
2020-01-30 18:23:43 -0500

    BMCDOT-498 Roadways CRUDS

--- app/models/roadway.rb
+++ app/models/roadway.rb
@@ -3,8 +3,9 @@ class Roadway < ApplicationRecord
   has_paper_trail
 
   # Callbacks
+  after_initialize :set_defaults
   after_save :reset_lanes_if_needed
-  
+
   belongs_to :highway_structure, foreign_key: :transam_asset_id
   belongs_to :route_signing_prefix
   belongs_to :service_level_type
@@ -14,8 +15,40 @@ class Roadway < ApplicationRecord
 
   has_many :roadbeds, dependent: :destroy
 
+  validates :route_signing_prefix, presence: true
+  validates :service_level_type, presence: true
+
   scope :on, -> { where(on_under_indicator: '1') }
   scope :under, -> { where.not(on_under_indicator: '1').order(:on_under_indicator) }
+
+  def self.allowable_params
+    [
+     :transam_asset_id,
+     :on_under_indicator,
+     :route_signing_prefix_id,
+     :service_level_type_id,
+     :route_number,
+     :features_intersected,
+     :facility_carried,
+     :min_vertical_clearance,
+     :milepoint,
+     :on_base_network,
+     :lrs_route,
+     :lrs_subroute,
+     :functional_class_id,
+     :lanes,
+     :average_daily_traffic,
+     :average_daily_traffic_year,
+     :total_horizontal_clearance,
+     :strahnet_designation_type_id,
+     :traffic_direction_type_id,
+     :on_national_highway_system,
+     :average_daily_truck_traffic_percent,
+     :on_truck_network,
+     :future_average_daily_traffic,
+     :future_average_daily_traffic_year
+    ]
+  end
 
   def reset_lanes_if_needed
     highway_structure.reset_lanes(self) if saved_change_to_attribute?(:lanes)
@@ -26,6 +59,43 @@ class Roadway < ApplicationRecord
       "On Structure - 1"
     else
       "Under Structure - #{on_under_indicator}"
+    end
+  end
+
+  def allowed_indicators
+    current_list = highway_structure.roadways.pluck(:on_under_indicator)
+    current_list.delete('')
+
+    disallow_two = highway_structure.roadways.count > 2
+    count = highway_structure.roadways.count
+    case count
+    when 0, 1, 2
+      list = ['1', '2'] - current_list
+    else
+      list = ['1'] + (0..(count - 2)).collect{|i| ('A'.ord + i).chr} - current_list
+    end
+
+    list << on_under_indicator if on_under_indicator.present?
+
+    list.sort
+  end
+
+  protected
+
+  def set_defaults
+    if highway_structure
+      last_indicator = highway_structure.roadways.pluck(:on_under_indicator).sort.last
+      self.on_under_indicator ||=
+        case last_indicator
+        when nil, ''
+          '1'
+        when '1'
+          '2'
+        when '2'
+          'B'
+        else
+          (last_indicator.ord + 1).chr
+        end
     end
   end
 end
--- spec/requests/inspections_spec.rb
+++ spec/requests/inspections_spec.rb
@@ -10,7 +10,11 @@ RSpec.describe Api::V1::InspectionsController, type: :request do
   let!(:test_inspection) { create(:bridge_condition, highway_structure: test_bridge.highway_structure, state: 'assigned', assigned_organization_id: test_user.organization_id) }
   let!(:test_culvert_inspection) { create(:culvert_condition, highway_structure: test_culvert.highway_structure, state: 'assigned', assigned_organization_id: test_user.organization_id) }
   # let!(:test_sign_inspection) { create(:ancillary_condition, highway_structure: test_sign.highway_structure, state: 'assigned', assigned_organization_id: test_user.organization_id) }
-  let!(:test_roadway) { create(:roadway, highway_structure: test_bridge.highway_structure) }
+  let!(:test_roadway) { create(:roadway,
+                               highway_structure: test_bridge.highway_structure,
+                               route_signing_prefix: create(:route_signing_prefix),
+                               service_level_type: create(:service_level_type)
+                               ) }
   let!(:test_element) { create(:element, inspection: test_inspection.inspection) }
   let!(:test_defect) { create(:defect, inspection: test_inspection.inspection, element: test_element) }
 
@@ -22,7 +26,7 @@ RSpec.describe Api::V1::InspectionsController, type: :request do
       test_user.organizations = [test_user.organization]
       test_user.viewable_organizations = [test_user.organization]
       test_user.save
-      get "/api/v1/inspections.json", headers: valid_headers 
+      get "/api/v1/inspections.json", headers: valid_headers
     }
 
     it 'returns data' do
@@ -32,13 +36,13 @@ RSpec.describe Api::V1::InspectionsController, type: :request do
       expect(json['data'].keys).to match_array(['inspections', 'elements', 'defects', 'defect_locations', 'structures', 'roadways', 'bridge_conditions', 'culvert_conditions', 'ancillary_conditions', 'images', 'documents', 'users', 'streambed_profiles', 'streambed_profile_points', 'roadbeds', 'roadbed_lines'])
     end
 
-    it 'includes structures data' do 
+    it 'includes structures data' do
       expect(json['data']['structures']).not_to be_empty
       expect(json['data']['structures']['bridges'].size).to eq(1)
       expect(json['data']['structures']['bridges'][0]["id"]).to eq(test_bridge.guid)
     end
 
-    it 'includes inspections data' do 
+    it 'includes inspections data' do
       expect(json['data']['inspections'].size).to eq(2)
       expect(json['data']['inspections'][0]["id"]).to eq(test_inspection.guid)
       expect(json['data']['inspections'][1]["id"]).to eq(test_culvert_inspection.guid)
@@ -60,11 +64,10 @@ RSpec.describe Api::V1::InspectionsController, type: :request do
     #   expect(json['data']['ancillary_conditions'][0]["id"]).to eq(test_sign_inspection.guid)
     # end
 
-    it 'includes roadways data' do 
+    it 'includes roadways data' do
       expect(json['data']['roadways'].size).to eq(1)
       expect(json['data']['roadways'][0]["id"]).to eq(test_roadway.guid)
     end
 
   end
 end
-
