59dc5b72cbd8c1beebf6fa3fca135e15e4486fa6
W. Scott Meeks <smeeks@camsys.com>
2020-02-27 16:32:33 -0500

    BMCDOT-600: image fields, short to_s option, json fields for photo log

--- app/models/defect_location.rb
+++ app/models/defect_location.rb
@@ -2,6 +2,10 @@ class DefectLocation < ApplicationRecord
 
   include TransamObjectKey
 
+  attr_accessor :global_base_imagable
+  attr_accessor :image
+  attr_accessor :exportable
+
   belongs_to :defect
   has_one :inspection, through: :defect
   has_many :images, as: :imagable, dependent: :destroy
@@ -16,7 +20,14 @@ class DefectLocation < ApplicationRecord
     ]
   end
 
-  def to_s
-    "#{quantity} #{defect.element.element_definition.quantity_unit} of #{condition_state} #{defect.defect_definition.short_name}: #{location_description}; #{note}"
+  def to_s(include_note=true)
+    "#{quantity} #{defect.element.element_definition.quantity_unit} of #{condition_state} #{defect.defect_definition.short_name}: #{location_description} #{include_note ? '; ' + note : ''}"
+  end
+
+    def as_json
+    super.merge!({
+                   "defect_number" => defect.defect_definition.number,
+                   "element_number" => defect.element.element_definition.number
+                 })
   end
 end
