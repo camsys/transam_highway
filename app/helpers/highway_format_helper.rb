11a52ef2aae733a77ae46d3a1b180c7391c23c8d
W. Scott Meeks <smeeks@camsys.com>
2020-02-27 16:31:05 -0500

    BMCDOT-600: helper and defect location links

--- app/helpers/highway_format_helper.rb
+++ app/helpers/highway_format_helper.rb
@@ -4,7 +4,7 @@ module HighwayFormatHelper
   end
 
   def reference_label(table_name, field_name, default_label)
-    FieldReference.get_label(table_name, field_name) || default_label 
+    FieldReference.get_label(table_name, field_name) || default_label
   end
 
   def suppress_values(value, suppress_all_zeroes: true, suppress_list: [-1, '-1', '_'], &block)
@@ -17,5 +17,16 @@ module HighwayFormatHelper
 
   def format_for_pdf_printing(datetime)
     datetime.strftime(" %m/%d/%Y @ %k:%M")
+  end
+
+  def defect_location_new_or_edit_path(defect, condition_state)
+    if defect.defect_locations.find_by(condition_state: condition_state)
+      edit_inspection_element_defect_defect_location_path(defect.inspection, defect.element,
+                                                          defect, defect.defect_locations.find_by(condition_state: condition_state),
+                                                          condition_state: condition_state)
+    else
+      new_inspection_element_defect_defect_location_path(defect.inspection, defect.element,
+                                                         defect, condition_state: condition_state)
+    end
   end
 end
