29a7504696a891af6926e42f990e727cb2b3c360
Alex Andrade <aandrade@camsys.com>
2020-04-03 16:59:09 -0400

    [BMCDOT-712] Assign inspection to roadbeds upon creation; show roadbeds related to inspection being viewed on roadbed tab; fix data migration for assigning inspections to existing roadbeds.

--- db/data_migrations/20200318212854_populate_inspection_ids_for_roadbeds.rb
+++
@@ -1,7 +0,0 @@
-class PopulateInspectionIdsForRoadbeds < ActiveRecord::DataMigration
-  def up
-    Roadbed.joins(:roadbed_lines).uniq.each do |r|
-      r.update(inspection: r.roadbed_lines.first.inspection)
-    end
-  end
-end
